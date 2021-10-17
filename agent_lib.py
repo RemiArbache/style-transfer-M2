014cbfc5d42dcc92bdea2e8261ce7c195c3fbaf0
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2021-10-17 19:29:43 +0200

    Suppress unnecessary printing while loading images

--- agent_lib.py
+++ agent_lib.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, render_template, send_file, Response
+from flask import Flask, request, render_template, send_file, Response, jsonify
 from io import BytesIO
 from PIL import Image
-
+import io
 import matplotlib.pylab as plt 
 import os
 import functools
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
+import contextlib
+import base64
 
 
 hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
@@ -40,12 +42,15 @@ def crop_center(image):
 @functools.lru_cache(maxsize=None)
 def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
     """Loads and preprocesses images."""
-    # Cache image file locally.
-    image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
-    # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
-    img = tf.io.decode_image(
-        tf.io.read_file(image_path),
-        channels=3, dtype=tf.float32)[tf.newaxis, ...]
-    img = crop_center(img)
-    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    with contextlib.redirect_stdout(io.StringIO()): # Suppress writing to console
+        
+        # Cache image file locally.
+        image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+        # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+        img = tf.io.decode_image(
+            tf.io.read_file(image_path),
+            channels=3, dtype=tf.float32)[tf.newaxis, ...]
+        img = crop_center(img)
+        img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+
     return img
